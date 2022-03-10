8ef2a7f1852482e8ade0eaa58d7a9fb60d08d2ff
Md Masum Ali <masumalihstu18@gmail.com>
2022-03-10 16:00:41 +0600

    MapList

--- bin/firstproject.dart
+++ bin/firstproject.dart
@@ -1,4 +1,21 @@
-void main(List<String> arguments) {
- var myName='My Name is Masum';
-  print(myName);
+void main(){
+ var myMap={
+  'name':'Masum',
+  'city':'ChapaiNawabganj',
+  'address':[
+   {
+    'country':'Bangladesh',
+    'city':'Dhaka'
+   },
+   {
+    'country':'Pakistan',
+    'city':'Islamabad'
+   }
+  ]
+ };
+ print(myMap['address']);
+ var addressList=myMap['address'];
+ (addressList as List).forEach((element) { 
+  print(element['country']);
+ });
 }
